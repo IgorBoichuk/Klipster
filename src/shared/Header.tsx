74e717e962478f2bdec4fa6b9261600033455fef
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-10-30 12:08:45 +0200

    fix config

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -3,7 +3,5 @@
 export const Header = () => {
 	// const pathname = usePathname().split('/')[1]
 
-	return (
-		<header className='sticky top-0 z-[100] bg-cwhite border-b py-[12px] xl:py-[24px] text-red-800'>Header</header>
-	);
+	return <header className='sticky top-0 z-[100] bg-cwhite border-b py-[12px] xl:py-[24px]'>Header</header>;
 };
