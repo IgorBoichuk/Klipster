dc2fb66fe97b7c35a79190e49c66dbe232c99a93
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-11-06 19:07:05 +0200

    headr for mob

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,16 +1,39 @@
 //import React from 'react';
 
+import Image from 'next/image';
 import { Container } from '../Container';
 import Link from 'next/link';
-import { NavNenu } from '../NavNenu';
+import Logo from '../../../public/logo/logo.svg';
+import Phone from '../../../public/svg/phone.svg';
+import Find from '../../../public/svg/find.svg';
+import Login from '../../../public/svg/login.svg';
+import BurgerMenu from '../../../public/svg/burger.svg';
 
 export const Header = () => {
 	// const pathname = usePathname().split('/')[1]
 
 	return (
-		<header className='sticky top-0 z-[100] bg-cdark border-b py-[12px] xl:py-[24px]'>
+		<header className='sticky top-0 z-[100] bg-cdark border-b '>
 			<Container>
-				<NavNenu></NavNenu>
+				<div className='py-4 flex justify-between items-center'>
+					<Link href={'/'} className='block w-[102px]'>
+						<Image src={Logo} alt='logo klipster' className=' ' />
+					</Link>
+					<div className='flex gap-3'>
+						<Link href={'tel:${+380930392656}'}>
+							<Image src={Phone} alt='phone link'></Image>
+						</Link>
+						<Link href={''}>
+							<Image src={Find} alt='Find link'></Image>
+						</Link>
+						<button>
+							<Image src={Login} alt='Login link'></Image>
+						</button>
+						<button>
+							<Image src={BurgerMenu} alt='BurgerMenu button'></Image>
+						</button>
+					</div>
+				</div>
 			</Container>
 		</header>
 	);
