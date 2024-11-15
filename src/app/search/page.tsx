164e9f60211695f52e93f7f6258cdcc1ca7dd83f
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-11-15 23:56:18 +0200

    add search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+import { SectionTitle } from '@/shared/SectionTitle';
+import React from 'react';
+
+const Search = () => {
+	return (
+		<div>
+			<SectionTitle title='Search' />
+		</div>
+	);
+};
+export default Search;
