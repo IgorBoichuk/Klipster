07ef7fbdde9d53f48561b7974ed9c58e7d76cb9d
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-11-22 15:01:20 +0200

    images from subdomen

--- src/shared/CategoryCard.tsx
+++ src/shared/CategoryCard.tsx
@@ -8,9 +8,13 @@ interface CategoryCardProps {
 }
 
 export const CategoryCard = ({ image, title }: CategoryCardProps) => {
+	const pathToCatalog = 'https://codex-dev.pro/';
+
 	return (
 		<div className='relative rounded-xl shadow-md overflow-hidden h-full'>
-			{image && <Image src={image} alt='Category card' width={500} height={500} className='' />}
+			{image && (
+				<Image src={`${pathToCatalog}${image}`} alt='Category card' width={500} height={500} unoptimized className='' />
+			)}
 			<div className='absolute flex justify-center items-center inset-x-0 bottom-0 bg-slate-500 bg-opacity-15 backdrop-blur-md rounded-b-xl w-full h-1/5'>
 				<p className='leading-none text-base text-center'>{title}</p>
 			</div>
