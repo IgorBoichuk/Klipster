93f10c829b861558f3423962308d936bb5aafd3f
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-11-30 13:01:20 +0200

    clean some files

--- src/helpers/fetchCategories.tsx
+++ src/helpers/fetchCategories.tsx
@@ -1,9 +1,31 @@
+// import { Product } from '@/types';
+
+// export const fetchCategories = async (): Promise<Product[] | undefined> => {
+// 	try {
+// 		const response = await fetch('/api/getData?table=categories');
+// 		const data: Product[] = await response.json();
+
+// 		if (Array.isArray(data)) {
+// 			return data;
+// 		} else {
+// 			console.error('Data is not an array', data);
+// 		}
+// 	} catch (error) {
+// 		console.error('Error fetching categories:', error);
+// 	}
+// };
+
+import axios from 'axios';
 import { Product } from '@/types';
 
-export const fetchCategories = async (): Promise<Product[] | undefined> => {
+const fetchCategoriesFromAPI = async (): Promise<Product[] | undefined> => {
 	try {
-		const response = await fetch('/api/getData?table=categories');
-		const data: Product[] = await response.json();
+		// Використовуємо axios для запиту
+		const response = await axios.get('/api/getData', {
+			params: { table: 'categories' }, // Передача параметрів у запит
+		});
+
+		const data: Product[] = response.data;
 
 		if (Array.isArray(data)) {
 			return data;
@@ -11,6 +33,9 @@ export const fetchCategories = async (): Promise<Product[] | undefined> => {
 			console.error('Data is not an array', data);
 		}
 	} catch (error) {
+		// Axios автоматично додає більше деталей у помилки
 		console.error('Error fetching categories:', error);
 	}
 };
+
+export default fetchCategoriesFromAPI;
--- src/shared/Sections.tsx
+++ src/shared/Sections.tsx
@@ -2,16 +2,32 @@
 import React, { useEffect, useState } from 'react';
 import { SectionTitle } from './SectionTitle';
 import { CategoryCard } from './CategoryCard';
-import { fetchCategories } from '@/helpers/fetchCategories';
+
 import { processSections } from '@/helpers/uniqueSections';
 import { Product } from '@/types';
+import fetchCategoriesFromAPI from '@/helpers/fetchCategories';
 
 export const Sections = () => {
 	const [sections, setSections] = useState<Product[]>([]);
 
+	// const fetchProducts = async () => {
+	// 	try {
+	// 		// Використання axios для отримання даних
+	// 		const response = await axios.get('/api/getData', {
+	// 			params: { table: 'partsitems' }, // Передача параметрів у запит
+	// 		});
+
+	// 		const data: Product[] = response.data;
+	// 		setProducts(data);
+	// 	} catch (error) {
+	// 		// Axios автоматично додає більше інформації про помилку
+	// 		console.error('Error fetching products:', error);
+	// 	}
+	// };
+
 	useEffect(() => {
 		const loadSections = async () => {
-			const data = await fetchCategories(); // Викликаємо функцію для отримання даних
+			const data = await fetchCategoriesFromAPI(); // Викликаємо функцію для отримання даних
 			if (data) {
 				setSections(data); // Зберігаємо дані в стейт
 			}
